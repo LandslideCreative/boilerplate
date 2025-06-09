bebe1cc5e68f4bdd7bd9d07b675b2b38a66e4695
Josh Jenkins <jsjenkins@gmail.com>
2025-06-09 11:08:28 -0500

    Event calendar fixes

--- wp-content/themes/landslide-theme/src/theme/inc/events.php
+++ wp-content/themes/landslide-theme/src/theme/inc/events.php
@@ -37,7 +37,7 @@ function ls_events_calendar_filter_relationship_result( $options, $field, $post_
 }
 add_filter('acf/fields/relationship/query/name=events', 'ls_events_calendar_filter_relationship_result', 10, 3);
 
-// Return 404 for past events
+// Return redirect/404 for past events
 function ls_expire_past_events() {
     if( function_exists('tribe_is_event') ) {
         global $wp_query;
@@ -50,10 +50,28 @@ function ls_expire_past_events() {
             $expiration_date = $expiration_date+86400+18000;
 
             if( $now->format('U') > $expiration_date ) {
-                $wp_query->set_404();
-                status_header( 404 );
-                include( get_query_template( '404' ) );
-                exit();
+
+                if( tribe_is_recurring_event() ) {
+                    $event_id = TEC\Events\Custom_Tables\V1\Models\Occurrence::normalize_id ( get_the_ID() );
+
+                    if($event_id) {
+                        $recurring_occurrence = tribe_events()->where( 'ID', $event_id )->where( 'start_date', $now->format('Y-m-d H:i:s') )->first()->ID;
+
+                        if( $recurring_occurrence ) {
+                            wp_redirect(tribe_get_event_link($recurring_occurrence));
+                        } else {
+                            $wp_query->set_404();
+                            status_header( 404 );
+                            include( get_query_template( '404' ) );
+                            exit();
+                        }
+                    }
+                } else {
+                    $wp_query->set_404();
+                    status_header( 404 );
+                    include( get_query_template( '404' ) );
+                    exit();
+                }
             }
         }
     }
@@ -65,7 +83,7 @@ function ls_remove_past_events_from_searchwp( $ids ) {
     if (function_exists('tribe_get_events')) {
         $past_events = tribe_get_events( [
            'end_date'     => 'now',
-           'post_per_page' => -1,
+           'posts_per_page' => -1,
         ] );
 
         foreach( $past_events as $past_event ) {
